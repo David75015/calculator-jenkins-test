2b4f4915261e4080102ccfd79abdc5762c7ee099
David75015 <noahdjoman@gmail.com>
2026-03-30 13:20:33 +0200

    first commit

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+import pytest
+from calculator import addition, division
+
+def test_addition():
+    assert addition(2, 3) == 5
+
+def test_division():
+    assert division(10, 2) == 5
+
+def test_division_by_zero():
+    with pytest.raises(ValueError):
+        division(10, 0)
